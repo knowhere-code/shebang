73a703b3d0880b86791b38670026ddbfa317c9de
SeregΔ <ametovsi@gmail.com>
2024-10-11 20:16:52 +0300

    Delete objstudio.sh

--- objstudio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PATH_TO_SCRIPT=$1
-
-[ -z "$PATH_TO_SCRIPT" ] || [ ! -f "$PATH_TO_SCRIPT" ] && echo "Path not found. sudo bash $0 /path/to/Last.rde" && exit 1
-
-/usr/lib/pyramid-control/ObjStudioConsole -s "$PATH_TO_RDE" -l "ServiceCore"
-

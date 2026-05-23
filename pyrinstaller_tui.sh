176b8d52155876af3cbec78cc514c5991f8da3be
Ametov <ametovsi@gmail.com>
2026-05-23 16:58:41 +0300

    добавлен перевод на EN

--- pyrinstaller_tui.sh
+++ pyrinstaller_tui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# TUI cкрипт для установки пакетов Пирамиды. Скрипт должен находиться в одной папке с пакетами.
+# TUI script for installing Pyramid 2.0 Net packages. The script must be located in the same folder as the packages.
+# author: Ametov S.I.
 
-# Проверка прав пользователя
 if [ "$(id -u)" != 0 ]; then
   echo "${TEXT_need_root} 'sudo $0'"
   sudo "$0" "$@"
@@ -17,45 +17,73 @@ FAILURE=1
 
 localize() {
     if test "$LANG" = "ru_RU.UTF-8" || test "$LANG" = "ru_RU.utf8"; then
-        PRODUCT_NAME="Пирамида 2.0"
-	TITLE="Установщик ${PRODUCT_NAME} beta"
-	BUTTON_next="Далее"
-	BUTTON_exit="Выход"
-	BUTTON_back="Назад"
-
-	SHORT_cs="Служба управления"
-	SHORT_col="Служба автоматизированного сбора данных"
-	SHORT_uw="Корпоративный веб-сервер"
-	SHORT_cw="Публичный веб-сервер"
-	SHORT_csp="Служба представления данных"
-	SHORT_int="Служба информобмена"
-	SHORT_usv="Служба синхронизации времени"
-	SHORT_opcc="Служба OPC UA клиентов"
-	SHORT_opcs="Служба OPC UA серверов"
-	TEXT_need_root="
-Для работы установщика требуются привилегии администратора"
-	TEXT_main_menu="Добро пожаловать в мастер установки ${PRODUCT_NAME}
+		PRODUCT_NAME="Пирамида 2.0"
+		TITLE="Установщик ${PRODUCT_NAME} beta"
+		BUTTON_next="Далее"
+		BUTTON_exit="Выход"
+		BUTTON_back="Назад"
+		MENU_install="Установка"
+		MENU_update="Обновление"
+		MENU_createdb="Создание базы данных PostgreSQL"
+		SHORT_cs="Служба управления"
+		SHORT_col="Служба автоматизированного сбора данных"
+		SHORT_uw="Корпоративный веб-сервер"
+		SHORT_cw="Публичный веб-сервер"
+		SHORT_csp="Служба представления данных"
+		SHORT_int="Служба информобмена"
+		SHORT_usv="Служба синхронизации времени"
+		SHORT_opcc="Служба OPC UA клиентов"
+		SHORT_opcs="Служба OPC UA серверов"
+		TEXT_need_root="Для работы установщика требуются привилегии администратора"
+		TEXT_main_menu="Добро пожаловать в мастер установки ${PRODUCT_NAME}
 
 Мастер установки позволит установить, обновить ${PRODUCT_NAME} с компьютера. Нажмите $BUTTON_next для продолжения или $BUTTON_exit для выхода из мастера установки."
 
-	TEXT_update_confirmation_tail="Нажмите $BUTTON_next, чтобы начать обновление. Чтобы вернуться и изменить настройки, нажмите $BUTTON_back."
-	TEXT_createdb_confirmation_tail="Нажмите $BUTTON_next, чтобы запустить скрипт создание базы данных Postgres. Чтобы вернуться и изменить настройки, нажмите $BUTTON_back."
-	TEXT_select_packages="Выберите набор для установки.
+		TEXT_update_confirmation_tail="Нажмите $BUTTON_next, чтобы начать обновление. Чтобы вернуться и изменить настройки, нажмите $BUTTON_back."
+		TEXT_createdb_confirmation_tail="Нажмите $BUTTON_next, чтобы запустить скрипт создание базы данных Postgres. Чтобы вернуться и изменить настройки, нажмите $BUTTON_back."
+		TEXT_select_packages="Выберите набор для установки.
 
 Не устанавливайте пакеты без необходимости: это усложнит настройку и может снизить производительность."
 
-	TEXT_license_ok="Лицензия успешно проверена"
-	TEXT_license_bad="Ошибка проверки лицензии, ключи не найдены. Ключи должны лежать в одной папке с дистрибутивами."
-	TEXT_installed_pyr="Службы ${PRODUCT_NAME} уже установлены. Попробуйте выполнить обновления"
-	TEXT_distr_pyr="Не найдены пакеты ${PRODUCT_NAME}! Установщик должен запускаться из папки с дистрибутивами"
-	TEXT_choose_activity="Выберите операцию, которую нужно выполнить."
+		TEXT_license_ok="Лицензия успешно проверена"
+		TEXT_license_bad="Ошибка проверки лицензии, ключи не найдены. Ключи должны лежать в одной папке с дистрибутивами."
+		TEXT_installed_pyr="Службы ${PRODUCT_NAME} уже установлены. Попробуйте выполнить обновления"
+		TEXT_distr_pyr="Не найдены пакеты ${PRODUCT_NAME}! Установщик должен запускаться из папки с дистрибутивами"
+		TEXT_choose_activity="Выберите операцию, которую нужно выполнить."
+		TEXT_anykey="Изучите лог на предмет ошибок! Для выхода в меню нажмите любую клавишу..."
+		TEXT_result_script="Скрипт выполнен. Код ошибки ${script_status}. Для выхода в главное меню нажите Ok."
     else	
 		PRODUCT_NAME="Pyramid 2.0"
-		TITLE="${PRODUCT_NAME} Setup beta"
+		TITLE="${PRODUCT_NAME} Installer beta"
 		BUTTON_next="Next"
 		BUTTON_exit="Exit"
 		BUTTON_back="Back"
-		TEXT_installed_pyr="Pyramid services are already installed! Try running the update script"
+		MENU_install="Installation"
+		MENU_update="Update"
+		MENU_createdb="Create PostgreSQL Database"
+		SHORT_cs="Control Service"
+		SHORT_col="Automated Data Collection Service"
+		SHORT_uw="Corporate Web Server"
+		SHORT_cw="Public Web Server"
+		SHORT_csp="Data Presentation Service"
+		SHORT_int="Information Exchange Service"
+		SHORT_usv="Time Synchronization Service"
+		SHORT_opcc="OPC UA Client Service"
+		SHORT_opcs="OPC UA Server Service"
+		TEXT_need_root="Administrator privileges are required to run the installer"
+		TEXT_main_menu="Welcome to the ${PRODUCT_NAME} Setup Wizard
+The setup wizard will help you install or update ${PRODUCT_NAME} on this computer. Click $BUTTON_next to continue or $BUTTON_exit to exit the setup wizard."
+		TEXT_update_confirmation_tail="Click $BUTTON_next to start the update. To go back and change settings, click $BUTTON_back."
+		TEXT_createdb_confirmation_tail="Click $BUTTON_next to run the Postgres database creation script. To go back and change settings, click $BUTTON_back."
+		TEXT_select_packages="Select the installation package set.
+Do not install packages unless necessary: this may complicate configuration and reduce performance."
+		TEXT_license_ok="License successfully verified"
+		TEXT_license_bad="License verification failed, keys not found. The keys must be located in the same folder as the distributions."
+		TEXT_installed_pyr="${PRODUCT_NAME} services are already installed. Try performing an update"
+		TEXT_distr_pyr="${PRODUCT_NAME} packages not found! The installer must be run from the distribution folder"
+		TEXT_choose_activity="Select the operation to perform."
+		TEXT_anykey="Check the log for errors! Press any key to return to the menu..."
+		TEXT_result_script="Script executed. Error code ${script_status}. Press Ok to return to the main menu."
     fi
 }
 
@@ -115,7 +143,7 @@ check_installed_pyr() {
 
 check_distr_pyr(){
 	if ls ./pyrnet-* &> /dev/null || ls ./pyramid-* &> /dev/null; then
-		echo "Пакеты для установки найдены!"
+		echo "Distribution packages found, starting the installer..."
 	else
 		whiptail --title  "$TITLE" --msgbox "${TEXT_distr_pyr}." "${HEIGHT}" "${WIDTH}"
 		exit "${FAILURE}"
@@ -124,7 +152,7 @@ check_distr_pyr(){
 
 notification() {
     whiptail --title "$TITLE" \
-        --msgbox "Скрипт выполнен. Код ошибки ${script_status}. Для выхода в главное меню нажите Ok." \
+        --msgbox "${TEXT_result_script}" \
         --ok-button "Ok" \
         "${HEIGHT}" "${WIDTH}"
 
@@ -154,8 +182,8 @@ createdb_notification(){
 }
 
 press_anykey(){
-	read -s -n 1 -p "Изучите лог на предмет ошибок! Для выхода в меню нажмите любую клавишу..."
-	echo -e ""
+	read -s -n 1 -p "${TEXT_anykey}"
+	echo -e
 }
 
 install_pyr_menu(){
@@ -197,9 +225,9 @@ main_menu(){
 	OPTION=$(whiptail --title "$TITLE" --menu "$TEXT_choose_activity" \
 	--cancel-button "$BUTTON_exit" \
 	"${HEIGHT}" "${WIDTH}" 3 --notags \
-	"1" "Установка" \
-	"2" "Обновление" \
-	"3" "Создание БД PostgreSQL" 3>&1 1>&2 2>&3)
+	"1" "$MENU_install" \
+	"2" "$MENU_update" \
+	"3" "$MENU_createdb" 3>&1 1>&2 2>&3)
 	
 	if [ "$?" -eq "${SUCCESS}" ] ; then
 		case "$OPTION" in
